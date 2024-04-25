a84a719432b725f769d8faf00ed82217af535799
Evan Earle <evan.earle@gmail.com>
2024-04-25 15:41:00 -0300

    designed single post page and css

--- src/app/blog/[slug]/page.jsx
+++ src/app/blog/[slug]/page.jsx
@@ -1,7 +1,43 @@
+import Image from "next/image";
+import styles from "./singlePost.module.css";
+
 const SinglePostPage = () => {
-    return (
-      <div>SinglePostPage</div>
-    )
-  }
-  
-  export default SinglePostPage
+  return (
+    <div className={styles.container}>
+      <div className={styles.imgContainer}>
+        <Image
+          className={styles.img}
+          src="https://images.pexels.com/photos/23105927/pexels-photo-23105927/free-photo-of-a-photo-of-the-ocean-with-waves-crashing.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+          alt=""
+          fill
+        />
+      </div>
+      <div className={styles.textContainer}>
+        <h1 className={styles.title}>Title</h1>
+        <div className={styles.detail}>
+          <Image
+            className={styles.avatar}
+            src="https://images.pexels.com/photos/16141843/pexels-photo-16141843/free-photo-of-pampas-grass-growing-against-blue-sky.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+            alt=""
+            width={50}
+            height={50}
+          />
+          <div className={styles.detailText}>
+            <span className={styles.detailTitle}>Author</span>
+            <span className={styles.detailValue}>Marcus Aurelius</span>
+          </div>
+          <div className={styles.detailText}>
+            <span className={styles.detailTitle}>Published</span>
+            <span className={styles.detailValue}>01.01.2024</span>
+          </div>
+        </div>
+        <div className={styles.content}>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam,
+          maiores quos velit, in esse, quia unde magni atque deleniti saepe
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default SinglePostPage;
