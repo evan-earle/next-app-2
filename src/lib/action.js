aebc8c5ac3f9c58b0f6095655620b0d7a5324bc6
Evan Earle <evan.earle@gmail.com>
2024-05-13 17:29:00 -0300

    moved register form to components

--- src/lib/action.js
+++ src/lib/action.js
@@ -1,9 +1,9 @@
 "use server";
 
-import { logging } from "../../next.config";
 import { signIn, signOut } from "./auth";
 import { User } from "./models";
 import { connectToDB } from "./utils";
+import bcrypt from "bcrypt";
 
 export const handleGithubLogin = async () => {
   "use server";
@@ -15,12 +15,12 @@ export const handleLogout = async () => {
   await signOut();
 };
 
-export const register = async (formData) => {
+export const register = async (previousState, formData) => {
   const { username, email, password, img, passwordRepeat } =
     Object.fromEntries(formData);
 
   if (password !== passwordRepeat) {
-    return "Passwords do not match";
+    return { error: "Passwords do not match" };
   }
   try {
     connectToDB();
@@ -28,18 +28,32 @@ export const register = async (formData) => {
     const user = await User.findOne({ username });
 
     if (user) {
-      return "Username already exists";
+      return { error: "User already exists" };
     }
+
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password, salt);
 
     const newUser = new User({
       username,
       email,
-      password,
+      password: hashedPassword,
       img,
     });
 
     await newUser.save();
-    console.log("saved to db");
+    return { success: true };
+  } catch (error) {
+    console.log(error);
+    return { error: "something went wrong" };
+  }
+};
+
+export const login = async (formData) => {
+  const { username, password } = Object.fromEntries(formData);
+
+  try {
+    await signIn("credentials", { username, password });
   } catch (error) {
     console.log(error);
     return { error: "something went wrong" };
--- src/lib/auth.js
+++ src/lib/auth.js
@@ -1,7 +1,34 @@
 import NextAuth from "next-auth";
 import GitHub from "next-auth/providers/github";
+import CredentialsProvider from "next-auth/providers/credentials";
 import { connectToDB } from "./utils";
 import { User } from "./models";
+import bcrypt from "bcrypt";
+
+const login = async (credentials) => {
+  try {
+    connectToDB();
+    const user = await User.findOne({ username: credentials.username });
+
+    if (!user) {
+      throw new Error("Wrong credentials");
+    }
+
+    const isPasswordCorrect = await bcrypt.compare(
+      credentials.password,
+      user.password
+    );
+
+    if (!isPasswordCorrect) {
+      throw new Error("Wrong credentials");
+    }
+    return user;
+  } catch (error) {
+    console.log(error);
+    throw new Error("Failed to login");
+  }
+};
+
 export const {
   handlers: { GET, POST },
   auth,
@@ -12,6 +39,17 @@ export const {
     GitHub({
       clientId: process.env.GITHUB_ID,
       clientSecret: process.env.GITHUB_SECRET,
+    }),
+    CredentialsProvider({
+      async authorize(credentials) {
+        try {
+          const user = await login(credentials);
+          return user;
+        } catch (error) {
+          console.log(error);
+          return null;
+        }
+      },
     }),
   ],
   callbacks: {
